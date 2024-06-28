bcba98c08cc0398759fc8f5f7a8005c1d1445855
stefexec <stefan.eckel@gmail.com>
2024-06-28 19:33:09 +0200

    added nofall debug msg

--- src/main/java/de/gurkenwerfer/dougdougclientclient/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/de/gurkenwerfer/dougdougclientclient/mixin/ServerPlayNetworkHandlerMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,6 +25,7 @@ public class ServerPlayNetworkHandlerMixin {
             if (ModuleManager.isModuleEnabled("NoFall")) {
                 if (mc.player != null) {
                     if (!mc.player.getAbilities().creativeMode && mc.player.getVelocity().y > -0.5) {
+                        mc.player.sendMessage(Text.of("Im Falling!"));
                         ((PlayerMoveC2SPacketAccessor) packet).setOnGround(true);
                     }
                 }
